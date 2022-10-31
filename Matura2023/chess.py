aca6436f04dab16e20522fb626a6b8fb7a2d71b1
Zeden9 <z3d3ns@gmail.com>
2022-10-31 20:06:26 +0100

    solving 1st exercise

--- Matura2023/chess.py
+++ Matura2023/chess.py
@@ -1,13 +1,32 @@
-import os
-
-myFile = open("Matura2023\szachy.txt", "r")
+myFile = open("szachy_przyklad.txt", "r")
 game = myFile.readlines()
 
-board = []
+boards = []
 for i in range(0, len(game), 9):
-    step = []
+    board = []
     for j in range(0, 8):
-        step.append(game[i+j])
-    board.append(step)    
+        board.append(game[i+j])
+    boards.append(board)
 
-print(board[5])
+occupiedSpaces = []
+for n in range(0, len(boards)):
+    occupiedSpace = []
+    for x in range(0,8):
+        xPusty = False
+        for y in range (0,8):
+            if(boards[n][y][x] != "."):
+                xPusty = True
+        if(xPusty):
+            occupiedSpace.append(x)
+    occupiedSpaces.append(occupiedSpace)
+
+clearCollumn = 0
+mostClear = 0
+for n in range(0, len(boards)):
+    if(len(occupiedSpaces[n])<8):
+        clearCollumn+=1
+        if(8-len(occupiedSpaces[n]) > mostClear):
+            mostClear = 8-len(occupiedSpaces[n])
+    
+
+print(clearCollumn, mostClear)
